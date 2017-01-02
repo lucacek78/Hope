9be9b59f33abeb36ad84121e4bc376b0a6e77a63
lucacek78 <lucacek@yahoo.it>
2017-01-02 21:34:03 +0100

    Hope
    Cordova for Arduino Bluetooth

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-bluetooth-serial.bluetoothSerial",
+        "file": "plugins/cordova-plugin-bluetooth-serial/www/bluetoothSerial.js",
+        "pluginId": "cordova-plugin-bluetooth-serial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-bluetooth-serial": "0.4.6",
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-console": "1.0.5"
+};
+// BOTTOM OF METADATA
+});
